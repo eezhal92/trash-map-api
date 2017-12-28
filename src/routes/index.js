163f4f28e904ca319aff5034ffb0cfcd5aac7afd
eezhal92 <arkinthesky.69@gmail.com>
2017-12-28 20:46:10 +0800

    use twilio for notification

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
+import Twilio from 'twilio'
 import express from 'express'
 
 import trashes from './trashes'
@@ -7,5 +8,24 @@ const router = express.Router()
 
 router.use('/trashes', trashes)
 router.use('/garbage-bins', garbageBins)
+router.post('/notif', (request, response, next) => {
+  const accountSid = process.env.TWILIO_SID
+  const authToken = process.env.TWILIO_AUTH_TOKEN
+  const fromNumber = process.env.TWILIO_NUMBER
+  const client = new Twilio(accountSid, authToken)
+
+  client.messages.create({
+    body: `${Date.now()} - Sampah di titik Palu Smart City telah penuh dan belum diambil selama 3 hari`,
+    to: '+6285225575696',
+    from: fromNumber
+  }).then((message) => {
+    response.json({
+      sid: message.sid
+    })
+  }).catch((err) => {
+    console.log(err)
+    next(err)
+  })
+})
 
 export default router
